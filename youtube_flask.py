7e33b3a02f01d7b07855363cab5315f943fe43ab
jigar-2005 <83760051+jigar-2005@users.noreply.github.com>
2021-05-10 13:30:20 +0530

    Update youtube_flask.py

--- youtube_flask.py
+++ youtube_flask.py
@@ -1,6 +1,3 @@
-# jab searchbar hide ho jaata h tab ek vo btn show hoti h uspr click krne pr searchbar input box aae jisme user searxh kr ske
-# searchbar jo open hoga vo un icon k upar hoga
-
 from youtube import create_app
 
 app = create_app()
